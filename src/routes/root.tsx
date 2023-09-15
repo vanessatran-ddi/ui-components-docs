54bf6c333e5de93708ee35ac2c829eda9c04690c
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2023-09-15 16:02:41 -0400

    Fix footer section.

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,56 +1,84 @@
-import { GoAAppFooter, GoAAppHeader, GoAMicrositeHeader, GoAOneColumnLayout } from "@abgov/react-components";
-import { useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import {
+    GoAAppFooter, GoAAppFooterMetaSection,
+    GoAAppFooterNavSection,
+    GoAAppHeader,
+    GoAMicrositeHeader,
+    GoAOneColumnLayout
+} from "@abgov/react-components";
+import {useEffect, useState} from "react";
+import {Link, Outlet} from "react-router-dom";
 
 import "./root.css"
 
-import { useLocation } from "react-router-dom";
+import {useLocation} from "react-router-dom";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+    const {pathname} = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
 
-  return null;
+    return null;
 }
 
 export default function Root() {
 
-  const [visible, setVisibility] = useState<boolean>(false);
+    const [visible, setVisibility] = useState<boolean>(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setVisibility(true)    
-    }, 50) 
-  })
-  
-  return (
-    <div className="app" style={{opacity: visible ? "1" : "0"}}>
-      <ScrollToTop />
-      <GoAOneColumnLayout>
-        <section slot="header">
-          <GoAMicrositeHeader type={"alpha"} feedbackUrl="https://github.com/GovAlta/ui-components/discussions" maxContentWidth="1500px" />
-          <GoAAppHeader heading="Design system" maxContentWidth="1500px">
-            <Link to="/get-started">Get started</Link>
-            <Link to="/guidelines">Guidelines</Link>
-            <Link to="/design-tokens">Design Tokens</Link>
-            <Link to="/components">Components</Link>
-            <Link to="/patterns">Patterns</Link>
-            <Link to="/serivce-toolkit" className="interactive">Service toolkit</Link>
-          </GoAAppHeader>
-        </section>
+    useEffect(() => {
+        setTimeout(() => {
+            setVisibility(true)
+        }, 50)
+    })
 
-        <section className="content">
-          <Outlet />
-        </section>
+    return (
+        <div className="app" style={{opacity: visible ? "1" : "0"}}>
+            <ScrollToTop/>
+            <GoAOneColumnLayout>
+                <section slot="header">
+                    <GoAMicrositeHeader type={"alpha"}
+                                        feedbackUrl="https://github.com/GovAlta/ui-components/discussions"
+                                        maxContentWidth="1500px"/>
+                    <GoAAppHeader heading="Design system" maxContentWidth="1500px">
+                        <Link to="/get-started">Get started</Link>
+                        <Link to="/patterns">Patterns and templates</Link>
+                        <Link to="/components">Components</Link>
+                        <Link to="/design-tokens">Styles</Link>
+                        <Link to="/content">Content</Link>
+                        <Link to="/support">Support</Link>
+                        <Link to="/report-a-bug" className="interactive">Report a bug</Link>
+                    </GoAAppHeader>
+                </section>
 
-        <section slot="footer">
-          <GoAAppFooter maxContentWidth="1500px" />
-        </section>
+                <section className="content">
+                    <Outlet/>
+                </section>
 
-      </GoAOneColumnLayout>
-    </div>
-  );
+                <section slot="footer">
+                    <GoAAppFooter maxContentWidth="1500px">
+                        <GoAAppFooterNavSection heading="Resources" maxColumnCount={2}>
+                            <Link to="/get-started">Get started</Link>
+                            <Link to="/patterns">Patterns and templates</Link>
+                            <Link to="/components">Components</Link>
+                            <Link to="/design-tokens">Styles</Link>
+                            <Link to="/ux-writing">UX writing</Link>
+                            <Link to="/contribute">Contribute</Link>
+                        </GoAAppFooterNavSection>
+                        <GoAAppFooterNavSection heading="Get support">
+                            <Link to="/submit-an-issue">Submit an issue</Link>
+                            <Link to="/support">#design-system-support</Link>
+                            <Link to="/drop-in-hours">Drop in hours</Link>
+                        </GoAAppFooterNavSection>
+                        <GoAAppFooterMetaSection>
+                            <Link to="/contribute-design-system">Contribute to the design system</Link>
+                            <Link to="/leave-feedback">Leave feedback</Link>
+                            <Link to="/release-notes">Release notes</Link>
+                        </GoAAppFooterMetaSection>
+                    </GoAAppFooter>
+                </section>
+
+            </GoAOneColumnLayout>
+        </div>
+    );
 }
