1b8f7e81bf1d42dc1432bc9b45ededb576550cf8
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2023-09-20 11:07:29 -0400

    Add content to layout page.

--- src/routes/design-tokens/layout/Layout.tsx
+++ src/routes/design-tokens/layout/Layout.tsx
@@ -9,6 +9,7 @@ import {
     GoATabs, GoAThreeColumnLayout, GoATwoColumnLayout
 } from "@abgov/react-components";
 import {Sandbox} from "@components/sandbox";
+import {CodeSnippet} from "@components/code-snippet/CodeSnippet.tsx";
 
 
 export default function LayoutPage() {
@@ -87,8 +88,7 @@ export default function LayoutPage() {
 
                         <h3>3 column layout</h3>
                         <Sandbox>
-                            <GoAThreeColumnLayout
-                                header={
+                            <GoAThreeColumnLayout header={
                                     <>
                                         <GoAMicrositeHeader type="alpha" version="UAT"/>
                                         <GoAAppHeader url="/" heading="Design System">
@@ -122,6 +122,79 @@ export default function LayoutPage() {
                                     <GoASkeleton type="card" size={2}/>
                                 </GoAGrid>
                             </GoAThreeColumnLayout>
+
+                            <CodeSnippet lang="typescript" tags="react" allowCopy={true} code={`
+          <GoAThreeColumnLayout header={
+                                    <>
+                                        <GoAMicrositeHeader type="alpha" version="UAT"/>
+                                        <GoAAppHeader url="/" heading="Design System">
+                                            <a href="/login">Sign in</a>
+                                        </GoAAppHeader>
+                                    </>}
+                                footer={<GoAAppFooter/>}
+                                nav={<>
+                                    <a href="#dashboard">Dashboard</a>
+                                    <a href="#accounts">Accounts</a>
+                                    <a href="#invoices">Invoices</a>
+                                    <a href="#employees">Employees</a>
+                                    <a href="#claims">Claims</a>
+                                </>}
+                                side-menu={<>
+                                    <h2>Side Menu</h2>
+                                    <p>Add in your content here.</p>
+                                </>
+                                }
+                            >
+                                <GoASkeleton type="header" size={4}/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="header" size={4}/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoAGrid minChildWidth="10ch">
+                                    <GoASkeleton type="card" size={2}/>
+                                    <GoASkeleton type="card" size={2}/>
+                                </GoAGrid>
+                            </GoAThreeColumnLayout>
+        `}/>
+                            <CodeSnippet lang="typescript" tags="angular" allowCopy={true} code={`
+  <goa-three-column-layout leftcolumnwidth="16ch">
+  <section slot="header">
+    <goa-microsite-header type="alpha" version="UAT" />
+    <goa-app-header url="/" heading="Design System">
+      <a href="/login">Sign in</a>
+    </goa-app-header>
+  </section>
+  <section slot="nav">
+    <a href="#dashboard">Dashboard</a>
+    <a href="#accounts">Accounts</a>
+    <a href="#invoices">Invoices</a>
+    <a href="#employees">Employees</a>
+    <a href="#claims">Claims</a>
+  </section>
+    <goa-skeleton type="header" size="4" />
+    <goa-skeleton type="text" />
+    <goa-skeleton type="text" />
+    <goa-skeleton type="text" />
+    <goa-skeleton type="header" size="4" />
+    <goa-skeleton type="text" />
+    <goa-skeleton type="text" />
+    <goa-skeleton type="text" />
+    <goa-grid minchildwidth="10ch">
+      <goa-skeleton type="card" size="2" />
+      <goa-skeleton type="card" size="2" />
+    </goa-grid>
+  <section slot="side-menu">
+    <h2>Side Menu</h2>
+    <p>Add in your content here</p>
+  </section>
+  <section slot="footer">
+    <goa-app-footer></goa-app-footer>
+  </section>
+</goa-three-column-layout>
+        `}/>
                         </Sandbox>
                     </GoATab>
                 </GoATabs>
