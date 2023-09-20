59ee73a35f8f2c21c27a21921d5e4b10bca0bdb2
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2023-09-20 10:46:48 -0400

    Add content to layout page.

--- src/routes/design-tokens/layout/Layout.tsx
+++ src/routes/design-tokens/layout/Layout.tsx
@@ -1,12 +1,12 @@
 import "./layout.css";
 import {
     GoAAppFooter,
-    GoAAppHeader,
+    GoAAppHeader, GoAGrid,
     GoAMicrositeHeader,
     GoAOneColumnLayout,
-    GoAPageBlock,
+    GoAPageBlock, GoASkeleton,
     GoATab,
-    GoATabs
+    GoATabs, GoAThreeColumnLayout, GoATwoColumnLayout
 } from "@abgov/react-components";
 import {Sandbox} from "@components/sandbox";
 
@@ -17,27 +17,115 @@ export default function LayoutPage() {
             <h1>Layout</h1>
             <h3>A layout is a structural template that supports consistency across applications by
                 defining visual grids, spacing, and sections.</h3>
+            <div className="layout">
+                <GoATabs>
+                    <GoATab heading="Code examples">
+                        <h3>1 column layout</h3>
+                        <Sandbox>
+                            <GoAOneColumnLayout>
+                                <section slot="header">
+                                    <GoAMicrositeHeader type="alpha" version="UAT"/>
+                                    <GoAAppHeader url="/" heading="Design System">
+                                        <a href="/login">Sign in</a>
+                                    </GoAAppHeader>
+                                </section>
+                                <GoAPageBlock width="704px">
 
-            <GoATabs>
-                <GoATab heading="Code examples">
-                    <h3>1 column layout</h3>
-                    <Sandbox>
-                        <GoAOneColumnLayout>
-                            <section slot="header">
-                                <GoAMicrositeHeader type="alpha" version="UAT"/>
-                                <GoAAppHeader url="/" heading="Design System">
-                                    <a href="/login">Sign in</a>
-                                </GoAAppHeader>
-                            </section>
-                            <GoAPageBlock width="704px">
-                            </GoAPageBlock>
-                            <section slot="footer">
-                                <GoAAppFooter/>
-                            </section>
-                        </GoAOneColumnLayout>
-                    </Sandbox>
-                </GoATab>
-            </GoATabs>
+                                    <GoASkeleton type="header" size={4}/>
+                                    <GoASkeleton type="text"/>
+                                    <GoASkeleton type="text"/>
+                                    <GoASkeleton type="text"/>
+                                    <GoASkeleton type="header" size={4}/>
+                                    <GoASkeleton type="text"/>
+                                    <GoASkeleton type="text"/>
+                                    <GoASkeleton type="text"/>
+                                    <GoAGrid minChildWidth="10ch">
+                                        <GoASkeleton type="card" size={2}/>
+                                        <GoASkeleton type="card" size={2}/>
+                                    </GoAGrid>
+                                </GoAPageBlock>
+                                <section slot="footer">
+                                    <GoAAppFooter/>
+                                </section>
+                            </GoAOneColumnLayout>
+                        </Sandbox>
+
+                        <h3>2 column layout</h3>
+                        <Sandbox>
+                            <GoATwoColumnLayout
+                                header={
+                                    <>
+                                        <GoAMicrositeHeader type="alpha" version="UAT"/>
+                                        <GoAAppHeader url="/" heading="Design System">
+                                            <a href="/login">Sign in</a>
+                                        </GoAAppHeader>
+                                    </>}
+                                footer={<GoAAppFooter/>}
+                                nav={<>
+                                    <a href="#dashboard">Dashboard</a>
+                                    <a href="#accounts">Accounts</a>
+                                    <a href="#invoices">Invoices</a>
+                                    <a href="#employees">Employees</a>
+                                    <a href="#claims">Claims</a>
+                                </>}
+                            >
+
+                                <GoASkeleton type="header" size={4}/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="header" size={4}/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoAGrid minChildWidth="10ch">
+                                    <GoASkeleton type="card" size={2}/>
+                                    <GoASkeleton type="card" size={2}/>
+                                </GoAGrid>
+                            </GoATwoColumnLayout>
+                        </Sandbox>
+
+                        <h3>3 column layout</h3>
+                        <Sandbox>
+                            <GoAThreeColumnLayout
+                                header={
+                                    <>
+                                        <GoAMicrositeHeader type="alpha" version="UAT"/>
+                                        <GoAAppHeader url="/" heading="Design System">
+                                            <a href="/login">Sign in</a>
+                                        </GoAAppHeader>
+                                    </>}
+                                footer={<GoAAppFooter/>}
+                                nav={<>
+                                    <a href="#dashboard">Dashboard</a>
+                                    <a href="#accounts">Accounts</a>
+                                    <a href="#invoices">Invoices</a>
+                                    <a href="#employees">Employees</a>
+                                    <a href="#claims">Claims</a>
+                                </>}
+                                side-menu={<>
+                                    <h2>Side Menu</h2>
+                                    <p>Add in your content here.</p>
+                                </>
+                                }
+                            >
+                                <GoASkeleton type="header" size={4}/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="header" size={4}/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoASkeleton type="text"/>
+                                <GoAGrid minChildWidth="10ch">
+                                    <GoASkeleton type="card" size={2}/>
+                                    <GoASkeleton type="card" size={2}/>
+                                </GoAGrid>
+                            </GoAThreeColumnLayout>
+                        </Sandbox>
+                    </GoATab>
+                </GoATabs>
+            </div>
         </>
     )
 }
